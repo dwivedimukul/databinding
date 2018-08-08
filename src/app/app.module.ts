61999217def62064bf4b1b55e752447687e5ed3c
Mukul <dwivedi.mukul940@gmail.com>
2018-08-08 16:42:23 +0530

    Module3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,15 @@ import { Mode1Component } from './module1/mode1/mode1.component';
 import { Module1Module } from './module1/module1.module';
 import { Mode2Component } from './module2/mode2/mode2.component';
 import { Module2Module } from './module2/module2.module';
+import { ParentComponent } from './module3/parent/parent.component';
+import { ChildComponent } from './module3/child/child.component';
+import { Module3Module } from './module3/module3.module';
 
 const appRoutes: Routes=[
   {path:'module1/mode1', component:Mode1Component},
-  {path:'module2/mode2',component:Mode2Component}
+  {path:'module2/mode2',component:Mode2Component},
+  {path:'module3/parent',component:ParentComponent},
+  {path:'module3/child',component:ChildComponent},
 ]
 @NgModule({
   declarations: [
@@ -17,7 +22,7 @@ const appRoutes: Routes=[
   
   ],
   imports: [
-    BrowserModule,Module1Module, RouterModule.forRoot(appRoutes),Module2Module
+    BrowserModule,Module1Module, RouterModule.forRoot(appRoutes),Module2Module,Module3Module
   ],
   providers: [],
   bootstrap: [AppComponent]
